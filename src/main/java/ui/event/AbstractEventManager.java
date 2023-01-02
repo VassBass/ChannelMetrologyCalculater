6fb13d11bd571f1c4f14b48e34b05f1aa70f3f4a
VassBassApp <vassbassapp@gmail.com>
2023-01-02 20:00:50 +0200

    Fix

--- src/main/java/ui/event/AbstractEventManager.java
+++ src/main/java/ui/event/AbstractEventManager.java
@@ -1,5 +1,14 @@
 package ui.event;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class AbstractEventManager {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractEventManager.class);
+
+    protected void notProcessed() {
+        logger.info("This action not processed!");
+    }
+
     public abstract void runEvent(Event event);
 }
--- src/main/java/ui/event/EventManager.java
+++ src/main/java/ui/event/EventManager.java
@@ -1,11 +1,16 @@
 package ui.event;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import static ui.event.EventManagerRegistrator.registration;
 
 public class EventManager {
+    private static final Logger logger = LoggerFactory.getLogger(EventManager.class);
+
     private final Map<String, AbstractEventManager> eventManagers = new HashMap<>();
 
     private static EventManager manager;
@@ -20,8 +25,12 @@ public class EventManager {
     }
 
     public void runEvent(EventSource eventSource, Event event) {
-        eventManagers
-                .getOrDefault(eventSource.getId(), DefaultEventManager.getInstance())
-                .runEvent(event);
+        String id = eventSource.getId();
+        AbstractEventManager manager = eventManagers.get(id);
+        if (manager == null) {
+            logger.info(String.format("EventManager for %s not found!", id));
+        } else {
+            manager.runEvent(event);
+        }
     }
 }
