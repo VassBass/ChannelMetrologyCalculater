665a4262eb24d3fd86beecc09831ba731e4c4500
VassBassApp <vassbassapp@gmail.com>
2023-01-17 11:23:53 +0200

    Add test

--- src/test/java/service/repository/config/SqliteRepositoryConfigHolderTest.java
+++ src/test/java/service/repository/config/SqliteRepositoryConfigHolderTest.java
@@ -12,22 +12,20 @@ import service.repository.repos.person.PersonRepository;
 import service.repository.repos.process.ProcessRepository;
 import service.repository.repos.sensor.SensorRepository;
 
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.junit.Assert.*;
 
 public class SqliteRepositoryConfigHolderTest {
 
     private SqliteRepositoryConfigHolder configHolder;
     private SqliteRepositoryConfigHolder configHolderTestInstance;
+    private SqliteRepositoryConfigHolder configHolderPropertiesFileNotExists;
 
     @Before
     public void setUp() {
         configHolder = new SqliteRepositoryConfigHolder();
         configHolderTestInstance = new SqliteRepositoryConfigHolder("properties/test_repository.properties");
-    }
-
-    @Test
-    public void testGetTestInstance() {
-        assertNotSame(configHolder, configHolderTestInstance);
+        configHolderPropertiesFileNotExists = new SqliteRepositoryConfigHolder(EMPTY);
     }
 
     @Test
@@ -37,18 +35,21 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(dbUrl, configHolder.getDBUrl());
         assertEquals(testDbUrl, configHolderTestInstance.getDBUrl());
+        assertTrue(configHolderPropertiesFileNotExists.getDBUrl().isEmpty());
     }
 
     @Test
     public void testGetUser() {
         assertTrue(configHolder.getUser().isEmpty());
         assertTrue(configHolderTestInstance.getUser().isEmpty());
+        assertTrue(configHolderPropertiesFileNotExists.getUser().isEmpty());
     }
 
     @Test
     public void testGetPassword() {
         assertTrue(configHolder.getPassword().isEmpty());
         assertTrue(configHolderTestInstance.getPassword().isEmpty());
+        assertTrue(configHolderPropertiesFileNotExists.getPassword().isEmpty());
     }
 
     @Test
@@ -57,6 +58,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(measurementTableName, configHolder.getTableName(MeasurementRepository.class));
         assertEquals(measurementTableName, configHolderTestInstance.getTableName(MeasurementRepository.class));
+        assertEquals(measurementTableName, configHolderPropertiesFileNotExists.getTableName(MeasurementRepository.class));
     }
 
     @Test
@@ -65,6 +67,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(sensorTableName, configHolder.getTableName(SensorRepository.class));
         assertEquals(sensorTableName, configHolderTestInstance.getTableName(SensorRepository.class));
+        assertEquals(sensorTableName, configHolderPropertiesFileNotExists.getTableName(SensorRepository.class));
     }
 
     @Test
@@ -73,6 +76,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(channelTableName, configHolder.getTableName(ChannelRepository.class));
         assertEquals(channelTableName, configHolderTestInstance.getTableName(ChannelRepository.class));
+        assertEquals(channelTableName, configHolderPropertiesFileNotExists.getTableName(ChannelRepository.class));
     }
 
     @Test
@@ -81,6 +85,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(departmentTableName, configHolder.getTableName(DepartmentRepository.class));
         assertEquals(departmentTableName, configHolderTestInstance.getTableName(DepartmentRepository.class));
+        assertEquals(departmentTableName, configHolderPropertiesFileNotExists.getTableName(DepartmentRepository.class));
     }
 
     @Test
@@ -89,6 +94,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(areaTableName, configHolder.getTableName(AreaRepository.class));
         assertEquals(areaTableName, configHolderTestInstance.getTableName(AreaRepository.class));
+        assertEquals(areaTableName, configHolderPropertiesFileNotExists.getTableName(AreaRepository.class));
     }
 
     @Test
@@ -97,6 +103,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(processTableName, configHolder.getTableName(ProcessRepository.class));
         assertEquals(processTableName, configHolderTestInstance.getTableName(ProcessRepository.class));
+        assertEquals(processTableName, configHolderPropertiesFileNotExists.getTableName(ProcessRepository.class));
     }
 
     @Test
@@ -105,6 +112,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(installationTableName, configHolder.getTableName(InstallationRepository.class));
         assertEquals(installationTableName, configHolderTestInstance.getTableName(InstallationRepository.class));
+        assertEquals(installationTableName, configHolderPropertiesFileNotExists.getTableName(InstallationRepository.class));
     }
 
     @Test
@@ -113,6 +121,7 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(calibratorTableName, configHolder.getTableName(CalibratorRepository.class));
         assertEquals(calibratorTableName, configHolderTestInstance.getTableName(CalibratorRepository.class));
+        assertEquals(calibratorTableName, configHolderPropertiesFileNotExists.getTableName(CalibratorRepository.class));
     }
 
     @Test
@@ -121,11 +130,13 @@ public class SqliteRepositoryConfigHolderTest {
 
         assertEquals(personTableName, configHolder.getTableName(PersonRepository.class));
         assertEquals(personTableName, configHolderTestInstance.getTableName(PersonRepository.class));
+        assertEquals(personTableName, configHolderPropertiesFileNotExists.getTableName(PersonRepository.class));
     }
 
     @Test
     public void testGetUnknownTableName() {
         assertTrue(configHolder.getTableName(SqliteRepositoryConfigHolderTest.class).isEmpty());
         assertTrue(configHolderTestInstance.getTableName(SqliteRepositoryConfigHolderTest.class).isEmpty());
+        assertTrue(configHolderPropertiesFileNotExists.getTableName(SqliteRepositoryConfigHolderTest.class).isEmpty());
     }
 }
