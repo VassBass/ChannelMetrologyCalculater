530b2c70d57931baeebe0d3890622268febfe703
VassBassApp <vassbassapp@gmail.com>
2023-03-04 18:14:53 +0200

    refactor

--- src/main/java/service/channel/info/SwingChannelInfoExecuter.java
+++ src/main/java/service/channel/info/SwingChannelInfoExecuter.java
@@ -1,12 +1,17 @@
 package service.channel.info;
 
 import model.dto.Channel;
+import model.ui.DialogWrapper;
+import model.ui.LoadingDialog;
 import repository.RepositoryFactory;
 import service.application.ApplicationScreen;
 import service.channel.info.ui.swing.SwingChannelInfoDialog;
 import service.root.ServiceExecuter;
+import util.ScreenPoint;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
 
 public class SwingChannelInfoExecuter implements ServiceExecuter {
 
@@ -20,15 +25,40 @@ public class SwingChannelInfoExecuter implements ServiceExecuter {
 
     @Override
     public void execute() {
-        RepositoryFactory repositoryFactory = RepositoryFactory.getInstance();
-        ApplicationScreen applicationScreen = ApplicationScreen.getInstance();
-        if (repositoryFactory != null && applicationScreen != null) {
-            SwingChannelInfoDialog dialog = new SwingChannelInfoDialog(applicationScreen);
-            ChannelInfoManager manager = new SwingChannelInfoManager(dialog, repositoryFactory);
+        new Worker().execute();
+    }
 
-            new SwingChannelInfoInitializer(dialog, manager, repositoryFactory, channel).init();
+    private class Worker extends SwingWorker<Void, Void> {
 
-            dialog.showing();
+        private final DialogWrapper loadingDialogWrapper;
+        private SwingChannelInfoDialog dialog;
+
+        private Worker() {
+            ApplicationScreen applicationScreen = ApplicationScreen.getInstance();
+            LoadingDialog loadingDialog = LoadingDialog.getInstance();
+            Point location = ScreenPoint.center(Objects.requireNonNull(applicationScreen), loadingDialog);
+            loadingDialogWrapper = new DialogWrapper(applicationScreen, loadingDialog, location);
+        }
+
+        @Override
+        protected Void doInBackground() {
+            RepositoryFactory repositoryFactory = RepositoryFactory.getInstance();
+            ApplicationScreen applicationScreen = ApplicationScreen.getInstance();
+            if (repositoryFactory != null && applicationScreen != null) {
+                dialog = new SwingChannelInfoDialog(applicationScreen);
+                ChannelInfoManager manager = new SwingChannelInfoManager(dialog, repositoryFactory);
+
+                new SwingChannelInfoInitializer(dialog, manager, repositoryFactory, channel).init();
+
+                dialog.showing();
+            }
+            return null;
+        }
+
+        @Override
+        protected void done() {
+            loadingDialogWrapper.shutdown();
+            if (dialog != null) dialog.showing();
         }
     }
 }
