59b3fc81b75bd5d244734f28930e0e0561ff9bc4
VassBassApp <vassbassapp@gmail.com>
2023-08-02 15:58:48 +0300

    Working

--- src/main/java/service/measurement/converter/ui/swing/SwingResultPanel.java
+++ src/main/java/service/measurement/converter/ui/swing/SwingResultPanel.java
@@ -6,14 +6,17 @@ import service.measurement.converter.ui.ResultPanel;
 public class SwingResultPanel extends TitledTextArea implements ResultPanel {
     private static final String TITLE_TEXT = "Результтат";
 
+    private final StringBuilder buffer;
+
     public SwingResultPanel() {
         super(Integer.MAX_VALUE, 50, TITLE_TEXT);
+        buffer = new StringBuilder();
     }
 
     @Override
     public void appendResult(String sourceMeasurementValue, double sourceValue, String resultMeasurementValue, double resultValue) {
-        String prefix = this.getText();
-        String postfix = String.format("%s %s = %s %s", sourceMeasurementValue, sourceValue, resultMeasurementValue, resultValue);
-        this.setText(prefix + postfix + '\n');
+        buffer.append(this.getText());
+        buffer.append(String.format("%s %s = %s %s%n", sourceMeasurementValue, sourceValue, resultMeasurementValue, resultValue));
+        this.setText(buffer.toString());
     }
 }
