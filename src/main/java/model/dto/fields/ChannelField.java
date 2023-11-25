83e50aa79477be1a28051d99ce10577a5fa9f8e8
ivasyliev <ivasyliev@unitedthinkers.com>
2023-11-25 13:08:57 +0200

    Removed unused fields

--- src/main/java/model/dto/fields/ChannelField.java
+++ src/main/java/model/dto/fields/ChannelField.java
@@ -19,25 +19,4 @@ public class ChannelField {
     public static final String ALLOWABLE_ERROR_VALUE = "allowableErrorValue";
     public static final String ALLOWABLE_ERROR_PERCENT = "allowableErrorPercent";
     public static final String SUITABILITY = "suitability";
-
-    public static class UI {
-        public static final String CODE = "Код каналу";
-        public static final String NAME = "Назва каналу";
-        public static final String MEASUREMENT_NAME = "Назва вимірювання";
-        public static final String MEASUREMENT_VALUE = "Величина вимірювання";
-        public static final String DEPARTMENT = "Цех";
-        public static final String AREA = "Дільниця";
-        public static final String PROCESS = "Секція, технологічна лінія, тощо";
-        public static final String INSTALLATION = "Установка";
-        public static final String DATE = "Дата";
-        public static final String FREQUENCY = "Періодичність перевірки";
-        public static final String TECHNOLOGY_NUMBER = "Технологічний номер";
-        public static final String NUMBER_OF_PROTOCOL = "Номер протоколу";
-        public static final String REFERENCE = "Довідка";
-        public static final String RANGE_MIN = "Мінімальна величина діапазону каналу";
-        public static final String RANGE_MAX = "Максимальна величина діапазону каналу";
-        public static final String ALLOWABLE_ERROR_VALUE = "Натуральна величина допустимої похибки каналу";
-        public static final String ALLOWABLE_ERROR_PERCENT = "Відносна величина допустимої похибки каналу";
-        public static final String SUITABILITY = "Придатність каналу";
-    }
 }
