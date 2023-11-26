f49c39ffbe056252dd0e69dc2b744c9b291201a5
ivasyliev <ivasyliev@unitedthinkers.com>
2023-11-26 09:21:57 +0200

    Fix

--- src/main/java/localization/label/UkraineLabels.java
+++ src/main/java/localization/label/UkraineLabels.java
@@ -8,45 +8,8 @@ public class UkraineLabels extends Labels {
 
     @Override
     protected void init() {
-        cancel = "Відміна";
-        importing = "Імпорт";
-        fileName = "Назва файлу";
-        typesOfFiles = "Типи файлів";
-        directory = "Директорія";
-        allFiles = "Всі файли";
-        closeWindow = "Закрити вікно";
-        open = "Відкрити";
-        openSelectedDirectory = "Відкрити обрану директорію";
-        openSelectedFile = "Відкрити обраний файл";
-        table = "Таблиця";
-        edited = "Редагувалося";
-        name = "Назва";
-        size = "Розмір";
-        type = "Тип";
-        toMain = "На головну";
-        list = "Список";
-        lists = "Списки";
-        createNewFolder = "Створити нову папку";
-        update = "Оновити";
-        upLevel = "На один рівень вверх";
-        view = "Відображення";
-        error = "Помилка";
-        start = "Почати";
-        checkDate = "Дата перевірки";
-        calculate = "Розрахувати";
-        back = "Назад";
-        close = "Закрити";
-        inputData = "Вхідні дані";
-        inputNotValid = "Помилковий ввод";
-        apply = "Застосувати";
-        print = "Друкувати";
-        done = "Готово";
-        further = "Далі";
-        invalidData = "Некоректні дані";
-
         //Calibrator
         calibratorsList = "Список калібраторів";
-        calibrator = "Калібратор";
 
         //Control points
         controlPointsOfMeasurements = "Контрольні точки вимірюваннь";
@@ -65,15 +28,7 @@ public class UkraineLabels extends Labels {
         sensorTypesList = "Список типів ПВП";
 
         //Channel calculation
-        calculatingOfMeasurementChannel_short = "Розрахунок ВК";
-        controlConditions = "Умови проведення контролю";
-        environmentTemperature = "Температура навколишнього середовища";
-        airHumidity = "Відносна вологість повітря";
-        atmospherePressure = "Атмосферний тиск";
         alarmCheck = "Перевірка сигналізації";
         alarmOnValue = "Величина при якій спрацювала сигналізація";
-
-        //Channel
-        protocolNumber = "Номер протоколу";
     }
 }
